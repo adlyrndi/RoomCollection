fa9d6d79e671750814da8c675dd9f8035013b2b8
Adly Renadi <adlyrenadi@Adlys-MacBook-Air.local>
2025-08-27 03:35:52 +0700

    fix(size): fixing size

--- app/components/mainPage/index.tsx
+++ app/components/mainPage/index.tsx
@@ -25,19 +25,19 @@ export default function MainPage() {
     <div className="w-full h-screen flex items-center justify-center">
       <div className="flex flex-col-reverse md:flex-row w-full max-w-7xl px-4 md:px-8 gap-40">
         {/* Left Section - Logo */}
-        <div className="flex items-center justify-center md:w-1/2 ">
+        <div className="flex items-center justify-center xl:justify-start md:w-1/2 pl-0 lg:pl-20">
           <Image
             src="/Logo-room.svg"
             alt="Logo"
             width={80}  // default untuk mobile
             height={80}
-            className="w-12 h-12 sm:w-20 sm:h-20 md:w-28 md:h-28 lg:w-32 lg:h-32"
+            className="w-12 h-12 sm:w-20 sm:h-20 md:w-28 md:h-28 lg:w-22 lg:h-22"
           />
         </div>
 
         {/* Right Section - Image + Text */}
         <div className="flex flex-col items-center justify-center text-center md:w-1/2 mt-15">
-          <div className="w-full relative h-[140px] sm:h-[250px] md:h-[300px] lg:h-[350px]">
+          <div className="w-full relative h-[140px] sm:h-[250px] md:h-[300px] lg:h-[250px]">
             {images.map((img, i) => (
               <Image
                 key={i}
