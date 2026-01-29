c7a53c6a7d2c8082d1ef01e26a19870ab80f73e5
Adly Renadi <adlyrenadi@Adlys-MacBook-Air.local>
2026-01-30 04:21:14 +0700

    fix(adsbar) : fixin adsbar

--- app/components/adsbar/index.tsx
+++ app/components/adsbar/index.tsx
@@ -4,9 +4,9 @@ export default function AdsBar() {
   const words = [
     // "OUR WEBSITE IS CURRENTLY STILL UNDER DEVELOPMENT ",
     // "──────",
-    "Pop-up at Parfume Pop Market, Brickhall Fatmawati, 30 Jan – 1 Feb 2026 — don’t miss it!",
+    "Pop-up at Perfume Pop Market, Brickhall Fatmawati, 30 Jan – 1 Feb 2026 — don’t miss it!",
     "──────",
-    "End of Year Sale is here! Get 30% off all items, Available on all marketplaces!.",
+    "Get 40% off at Perfume Pop Market.",
     "──────",
   ];
 
